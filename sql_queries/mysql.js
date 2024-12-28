e4325fefb46dafed8887e6e7cccda45b7c76b2c8
Sharan Kudtarkar <SK19@ador.co.in>
2024-12-28 11:39:55 +0530

    28-12-2024---11-39-49

--- sql_queries/mysql.js
+++ sql_queries/mysql.js
@@ -1,28 +1,40 @@
 exports.breakdown_machines_query = (start_date, end_date, list) => {
-    let _list = list.map(value => `'${value}'`).join(", ");
-_list=[...list,0]
-        const q = `
-        select * from (
-        SELECT
-        voae.*,
-        voeb.status,voeb.creation_date
-    FROM
-        view_of_all_equipment voae
-    LEFT JOIN
-        view_of_equipment_breakdown voeb
-        ON voae.equipment_id = voeb.equipment_id
-        where voeb.creation_date is not null
-            AND voeb.creation_date >= '${start_date} 00:00:00'
-            AND voeb.creation_date <= '${end_date} 23:59:59'
-    ) t1 where equipment_id not in (${_list}) order by t1.department_id,t1.section,t1.equipment
-`
-    return q
+    try {
+        console.log({ start_date, end_date, list });
+        
+        let _list = list.map(value => `'${value}'`).join(", ");
+    _list=[...list,0]
+            const q = `
+            select * from (
+            SELECT
+            voae.*,
+            voeb.status,voeb.creation_date
+        FROM
+            view_of_all_equipment voae
+        LEFT JOIN
+            view_of_equipment_breakdown voeb
+            ON voae.equipment_id = voeb.equipment_id
+            where voeb.creation_date is not null
+                AND voeb.creation_date >= '${start_date} 00:00:00'
+                AND voeb.creation_date <= '${end_date} 23:59:59'
+        ) t1 where equipment_id not in (${_list}) order by t1.department_id,t1.section,t1.equipment
+    `
+        return q
+    } catch (error) {
+        console.log("error in breakdown_machines_query");
+        
+    }
 }
 
 
 exports.list_of_section_id = () => {
-    const q = `
-    select * from view_of_all_equipment voae
-    `
-    return  q
+    try {
+        const q = `
+        select * from view_of_all_equipment voae
+        `
+        return  q
+    } catch (error) {
+        console.log("error in list_of_section_id");
+        
+    }
 }
