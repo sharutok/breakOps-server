5bbcbf497586df63555890bf5e535e6604f56faf
Sharan Kudtarkar <SK19@ador.co.in>
2025-01-14 12:21:27 +0530

    14-01-2025---12-21-14

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,9 @@
-const { AutomigData, ListOfIgnoredSectionId, getIgnoredEquipmentId, setIgnoredEquipmentId, AutomigCachedData, } = require('../Controller/Automig')
+const { AutomigData, ListOfIgnoredSectionId, getIgnoredEquipmentId, setIgnoredEquipmentId, AutomigCachedData, AutomigFetchedData, } = require('../Controller/Automig')
 
 const router = require('express').Router()
 
 router.post('/dashboard', AutomigCachedData)
+router.post('/generate-dashboard', AutomigFetchedData)
 router.get('/list-of-section-id', ListOfIgnoredSectionId)
 router.get('/list-of-rejected-equipment-id', getIgnoredEquipmentId)
 router.post('/list-of-rejected-equipment-id', setIgnoredEquipmentId)
